24bbe3dca562da421287a39d8d5b7d55c74e79b9
arunpunithkumar3-a11y <arunpunithkumar3@gmail.com>
2026-04-14 11:13:32 +0530

    updated

---
+++ auth.py
@@ -0,0 +1,31 @@
+import os
+import dotenv
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import requests
+
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send',
+    'https://www.googleapis.com/auth/gmail.modify',
+    'https://www.googleapis.com/auth/gmail.compose',
+]
+
+
+
+def get_gmail_service(user_data: dict):
+    creds = Credentials(
+        token=user_data["access_token"],
+        refresh_token=user_data["refresh_token"],
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+    )
+
+    if creds.expired:
+        creds.refresh(Request())
+
+    return build("gmail", "v1", credentials=creds)
